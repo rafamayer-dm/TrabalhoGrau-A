705e66f35d551b9b9c104a0392b8a551ee6859e0
CaioTBTW <caioagrassilva@gmail.com>
2026-04-07 21:37:19 -0300

    dfsdf

--- testecodigo.py
+++ testecodigo.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
+# oiiiiii
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 x, y, z = np.indices((8,8,8))
